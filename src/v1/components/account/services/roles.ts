e329f0ddd2025a62cb21377412645a228c83cbb6
Dinh Duy Phuoc <ddphuoc19@apcs.fitus.edu.vn>
2023-07-05 23:33:12 +0700

    Edit

--- src/v1/components/account/services/roles.ts
+++
@@ -1,5 +0,0 @@
-export enum UserRole {
-  ADMIN = "admin",
-  LECTURER = "lecturer",
-  STUDENT = "student",
-}
--- src/v1/utils/common.ts
+++
@@ -1,4 +0,0 @@
-export const getCurrentDatetime = () => {
-  // get ISO-8601 formatted datetime
-  return new Date().toISOString();
-};
