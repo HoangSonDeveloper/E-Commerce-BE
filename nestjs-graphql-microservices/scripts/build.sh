f4a3eccbd298c5e01ae179c5fc6a592bfde11bee
Duy-Phuoc Dinh <duyphuoc123234@gmail.com>
2023-08-04 08:46:51 +0700

    Delete nestjs-graphql-microservices directory

--- nestjs-graphql-microservices/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd api-gateway && npm run copy:protos && npm run build && cd -
-cd microservices/comments-svc && npm run copy:protos && npm run build && cd -
-cd microservices/posts-svc && npm run copy:protos && npm run build && cd -
-cd microservices/users-svc && npm run copy:protos && npm run build && cd -
-cd microservices/mailer-svc && npm run copy:protos && npm run build && cd -
-docker-compose build --no-cache
--- nestjs-graphql-microservices/scripts/generate-proto-docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-  -v $PWD/docs/proto:/out \
-  -v $PWD/_proto:/protos \
-  pseudomuto/protoc-gen-doc --doc_opt=markdown,docs.md
